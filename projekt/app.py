604c7cf9c41236a61a9dc502289ecf76eb027e2c
lukasculac <100538312+lukasculac@users.noreply.github.com>
2022-02-28 22:15:46 +0100

    Update app.py

--- projekt/app.py
+++ projekt/app.py
@@ -1,3 +1,4 @@
+from this import d
 from tkinter import *
 from  tkinter import ttk
 import tkinter
@@ -8,9 +9,14 @@ import threading
 from time import sleep
 
 port = "COM3"
-i = 0
 data  = []
 buffer = [20,1010,500,100.00]
+table = [
+    ["Proba", ""],
+    ["Test", ""]
+]
+global count
+count=0
 
 def conn():
     global data
@@ -52,6 +58,56 @@ def values():
     sec.insert(END, " %") 
     sec.insert(END, "\n")
     root.after(100, values)
+    
+
+def heating():
+    if(float(buffer[0]) < 20):
+        return "uključeno"
+    else:
+        return "isključeno"
+def cooling():
+    if(float(buffer[0]) > 29):
+        return "uključeno"
+    else:
+        return "isključeno"
+def lights():
+    if(float(buffer[2]) > 800):
+        return "upaljeno"
+    else:
+        return "ugašeno"
+def pressure():
+    if(float(buffer[1]) < 1010):
+        return "loše"
+    elif(float(buffer[1]) > 1019):
+        return "dobro"
+    elif(float(buffer[1]) > 1010 and float(buffer[1]) < 1019):
+        return "neodređeno"
+def humid():
+    if(float(buffer[3]) < 40):
+        return "uključeno"
+    else:
+        return "isključeno"
+def dehumid():
+    if(float(buffer[3]) > 60):
+        return "uključeno"
+    else:
+        return "isključeno"
+def tablica():
+    
+    grijanje = Label(tbl_frame,text=heating(), font=("Tahoma", 16), relief=SUNKEN)
+    grijanje.grid(row=0, column=1)
+    hladjenje = Label(tbl_frame, text=cooling(), font=("Tahoma", 16), relief=SUNKEN )
+    hladjenje.grid(row=1, column=1)
+    osvjetljenje = Label(tbl_frame, text=lights(), font=("Tahoma", 16), relief=SUNKEN )
+    osvjetljenje.grid(row=2, column=1)
+    ovlazivanje = Label(tbl_frame, text=humid(), font=("Tahoma", 16), relief=SUNKEN )
+    ovlazivanje.grid(row=3, column=1)
+    odvlazivanje = Label(tbl_frame, text=dehumid(), font=("Tahoma", 16), relief=SUNKEN )
+    odvlazivanje.grid(row=4, column=1)
+    vrijeme = Label(tbl_frame, text=pressure(), font=("Tahoma", 16), relief=SUNKEN )
+    vrijeme.grid(row=5, column=1) 
+    
+    root.after(100, tablica)
 
 root = Tk()
 root.title('Pametni stan')
@@ -60,20 +116,39 @@ root['bg'] = '#C1DBE3'
 
 var = StringVar(root)
 
+#Naslov
 l = Label(root, text = "Vrijednosti parametara")
 l.config(font = ("Courier", 15))
 l.pack(pady=10)
 
+#Izmjereni podaci
 glavni = ttk.Label(
     root,
     text='',
     font=("Arial", 18),
     textvariable=var)
 glavni.pack(ipadx=10, ipady=10, pady=10)
-
 sec = Entry(root, textvariable = var)
 
 
+#tablica
+tbl_frame = Frame(root)
+tbl_frame.pack()
+
+grijanje = Label(tbl_frame,text="GRIJANJE", font=("Tahoma", 17))
+grijanje.grid(row=0, column=0)
+hladjenje = Label(tbl_frame, text="HLAĐENJE", font=("Tahoma", 17))
+hladjenje.grid(row=1, column=0)
+osvjetljenje = Label(tbl_frame, text="RASVJETA", font=("Tahoma", 17))
+osvjetljenje.grid(row=2, column=0)
+ovlazivanje = Label(tbl_frame, text="OVLAŽIVANJE", font=("Tahoma", 17))
+ovlazivanje.grid(row=3, column=0)
+odvlazivanje = Label(tbl_frame, text="ODVLAŽIVANJE", font=("Tahoma", 17))
+odvlazivanje.grid(row=4, column=0)
+vrijeme = Label(tbl_frame, text="VRIJEME", font=("Tahoma", 17))
+vrijeme.grid(row=5, column=0)
+
+tablica()
 values()
 
 root.mainloop()

d60f1444d79992ece20248e4eb07ed0d49c12592
lukasculac <100538312+lukasculac@users.noreply.github.com>
2022-03-02 17:57:35 +0100

    Update app.py

--- projekt/app.py
+++ projekt/app.py
@@ -1,12 +1,18 @@
-from this import d
 from tkinter import *
 from  tkinter import ttk
-import tkinter
 from tkinter import font
+import tkinter as tk
 import serial
 import time
 import threading
 from time import sleep
+import numpy as np
+import random
+from itertools import count
+from matplotlib import pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 port = "COM3"
 data  = []
@@ -15,8 +21,6 @@ table = [
     ["Proba", ""],
     ["Test", ""]
 ]
-global count
-count=0
 
 def conn():
     global data
@@ -36,7 +40,6 @@ def conn():
         buffer[2] = data[2]
         buffer[3] = data[3]
     
-
 def values():
     conn()
     #print(buffer) 
@@ -59,6 +62,29 @@ def values():
     sec.insert(END, "\n")
     root.after(100, values)
     
+def animate(i):
+    yar.append(int(float(buffer[2])))
+    xar.append(i)
+    line.set_data(xar, yar)
+    ax1.set_xlim(0, i+1)
+
+def animate_temp(i):
+    yar_temp.append(int(float(buffer[0])))
+    xar_temp.append(i)
+    line_temp.set_data(xar_temp, yar_temp)
+    ax1_temp.set_xlim(0, i+1)
+
+def animate_pre(i):
+    yar_pre.append(int(float(buffer[1])))
+    xar_pre.append(i)
+    line_pre.set_data(xar_pre, yar_pre)
+    ax1_pre.set_xlim(0, i+1)
+
+def animate_hum(i):
+    yar_hum.append(int(float(buffer[3])))
+    xar_hum.append(i)
+    line_hum.set_data(xar_hum, yar_hum)
+    ax1_hum.set_xlim(0, i+1)
 
 def heating():
     if(float(buffer[0]) < 20):
@@ -71,7 +97,7 @@ def cooling():
     else:
         return "isključeno"
 def lights():
-    if(float(buffer[2]) > 800):
+    if(float(buffer[2]) > 500):
         return "upaljeno"
     else:
         return "ugašeno"
@@ -93,7 +119,6 @@ def dehumid():
     else:
         return "isključeno"
 def tablica():
-    
     grijanje = Label(tbl_frame,text=heating(), font=("Tahoma", 16), relief=SUNKEN)
     grijanje.grid(row=0, column=1)
     hladjenje = Label(tbl_frame, text=cooling(), font=("Tahoma", 16), relief=SUNKEN )
@@ -109,12 +134,45 @@ def tablica():
     
     root.after(100, tablica)
 
+
 root = Tk()
 root.title('Pametni stan')
-root.geometry('1200x700')
+root.geometry('400x700+50+50')
 root['bg'] = '#C1DBE3'
-
+xar = []
+yar = []
+xar_temp = []
+yar_temp = []
+xar_pre = []
+yar_pre = []
+xar_hum = []
+yar_hum = []
 var = StringVar(root)
+newwindow = tk.Toplevel(root)
+newwindow.geometry("1500x500+500+300")
+
+style.use('ggplot')
+#plot_svjetlost
+fig = plt.figure(figsize=(14, 4.5), dpi=50)
+ax1 = fig.add_subplot(1, 1, 1)
+ax1.set_ylim(0, 1000)
+line, = ax1.plot(xar, yar, 'r', marker='o')
+#plot_temp
+fig_temp = plt.figure(figsize=(14, 4.5), dpi=50)
+ax1_temp = fig_temp.add_subplot(1, 1, 1)
+ax1_temp.set_ylim(0, 50)
+line_temp, = ax1_temp.plot(xar_temp, yar_temp, 'r', marker='o')
+#plot_pre
+fig_pre = plt.figure(figsize=(14, 4.5), dpi=50)
+ax1_pre = fig_pre.add_subplot(1, 1, 1)
+ax1_pre.set_ylim(900, 1200)
+line_pre, = ax1_pre.plot(xar_pre, yar_pre, 'r', marker='o')
+#plot_hum
+fig_hum = plt.figure(figsize=(14, 4.5), dpi=50)
+ax1_hum = fig_hum.add_subplot(1, 1, 1)
+ax1_hum.set_ylim(0, 100)
+line_hum, = ax1_hum.plot(xar_hum, yar_hum, 'r', marker='o')
+
 
 #Naslov
 l = Label(root, text = "Vrijednosti parametara")
@@ -129,7 +187,6 @@ glavni = ttk.Label(
     textvariable=var)
 glavni.pack(ipadx=10, ipady=10, pady=10)
 sec = Entry(root, textvariable = var)
-
 
 #tablica
 tbl_frame = Frame(root)
@@ -151,5 +208,27 @@ vrijeme.grid(row=5, column=0)
 tablica()
 values()
 
+#svjetlost
+plotcanvas = FigureCanvasTkAgg(fig, newwindow)
+plotcanvas.get_tk_widget().grid(column=1, row=1)
+ani = animation.FuncAnimation(fig, animate, interval=1000, blit=False)
+
+#temp
+plotcanvas_temp = FigureCanvasTkAgg(fig_temp, newwindow)
+plotcanvas_temp.get_tk_widget().grid(column=1, row=2)
+ani_temp = animation.FuncAnimation(fig_temp, animate_temp, interval=1000, blit=False)
+
+#pressure
+plotcanvas_pre = FigureCanvasTkAgg(fig_pre, newwindow)
+plotcanvas_pre.get_tk_widget().grid(column=2, row=1)
+ani_pre = animation.FuncAnimation(fig_pre, animate_pre, interval=1000, blit=False)
+
+#humidity
+plotcanvas_hum = FigureCanvasTkAgg(fig_hum, newwindow)
+plotcanvas_hum.get_tk_widget().grid(column=2, row=2)
+ani_hum = animation.FuncAnimation(fig_hum, animate_hum, interval=1000, blit=False)
+
+
 root.mainloop()
+
 
